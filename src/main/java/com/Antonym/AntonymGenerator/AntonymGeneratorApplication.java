95b0ad5c9adc35c14d54adad1e8a70167c67341d
T Sai Deepak <tsaideepak7@gmail.com>
2023-01-28 19:11:32 +0530

    project init

---
+++ src/main/java/com/Antonym/AntonymGenerator/AntonymGeneratorApplication.java
@@ -0,0 +1,13 @@
+package com.Antonym.AntonymGenerator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AntonymGeneratorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AntonymGeneratorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Antonym/AntonymGenerator/AntonymGeneratorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Antonym.AntonymGenerator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AntonymGeneratorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
